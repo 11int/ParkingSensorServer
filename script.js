ac69f6354714cc0b2f0a0d7d6fafd68f7b16cc06
11int <miladd2006@gmail.com>
2023-04-25 15:14:45 +0200

    update 6

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const distanceElement = document.getElementById("distance");
 
+async function logJSONData() {
+  const response = await fetch("http://192.168.2.233:8000/sensor/1");
+  const jsonData = await response.text();
+  console.log(jsonData);
+}
+
+logJSONData();
+
 function getSensorData() {
   // replace this with your code to read sensor data
-  return Math.floor(Math.random() * 100);
 }
 
 setInterval(() => {
