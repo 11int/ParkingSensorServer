3f0233295b883e765a3038ff9f05c0d253edad67
11int <miladd2006@gmail.com>
2023-04-25 12:52:51 +0200

    Update 3

--- html.js
+++ html.js
@@ -1,14 +1,51 @@
 const http = require("http");
+const fs = require("fs");
 
 const host = 'localhost';
 const port = 8000;
 
-const requestListener = function (req, res) {};
-let status = document.getElementById("status");
+const requestListener = function (req, res) {
+    if (req.url === '/') {
+        fs.readFile(__dirname + '/index.html', (err, data) => {
+            if (err) {
+                res.writeHead(404);
+                res.end(JSON.stringify(err));
+                return;
+            }
+            res.writeHead( 200);
+            res.end(data);
+        });
+    } else if (req.url === '/script.js') {
+        fs.readFile(__dirname + '/sript.js', (err, data) => {
+            if (err) {
+                res.writeHead(404);
+                res.end(JSON.stringify(err));
+                return;
+            }
+            res.writeHead(200, { 'Content-Type': 'text/javascript' });
+            res.end(data);
+        });
+    } else {
+        res.writeHead(404);
+        res.end();
+    }
+};
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
-    status.innerHTML = "Hello World";
-
 });
+
+const express = require('express')
+const app = express()
+
+app.get('/sensor', (req, res) => {
+  // Write the logic for your endpoint here
+  const distance = getSensorData() // Replace getSensorData() with the function that gets the sensor data
+  res.send(`Distance: ${distance} cm`)
+})
+
+app.listen(8000, () => {
+  console.log('Server started on port 8000')
+})
+
