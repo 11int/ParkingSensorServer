d01becbe8d480122540edc4f0bb93163f2ba569e
11int <miladd2006@gmail.com>
2023-05-23 09:24:37 +0200

    MomentUpdate

--- api.js
+++ api.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors');
 const app = express();
 const moment = require('moment');
+require('moment-timezone');
 app.use(cors())
 
 app.use(bodyParser.json());
@@ -24,7 +25,7 @@ app.post("/sensor", (req, res) => {
     const data = req.body;
     console.log(data.sensorId);
     const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-    const currentTime = moment().tz(timezone).format();
+    const currentTime = moment.tz(timezone).format();
     const sensorData = {
       distanceCm: data.distanceCm,
       timestamp: currentTime, 
